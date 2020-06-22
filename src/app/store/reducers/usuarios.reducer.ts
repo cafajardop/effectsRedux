34f07ed8349fd1609d01d310e21d8d7af6500580
carlos andres fajardo pedraza <carlosafp84@hotmail.com>
2020-06-22 01:09:19 -0500

    Finalizando la clase 145 implementacion de los reducers y los actions

---
+++ src/app/store/reducers/usuarios.reducer.ts
@@ -0,0 +1,40 @@
+import { createReducer, on } from '@ngrx/store';
+import { cargarUsuarios, cargarUsuariosError, cargarUsuariosSuccess } from '../actions';
+
+export interface UsuariosState {
+    users: [],
+    loaded: boolean,
+    loading: boolean,
+    error: any
+}
+
+export const usuariosInitialState: UsuariosState = {
+    users: [],
+    loaded: false,
+    loading: false,
+    error: null
+}
+
+const _counterReducer = createReducer(usuariosInitialState,
+
+    on(cargarUsuarios, state => ({ ...state, loading: true })),
+    
+    on(cargarUsuariosSuccess, (state,{ usuarios }) => ({
+        ...state,
+        loading: false,
+        loaded: true,
+        users: [ ...usuarios ]
+    })),
+
+    on(cargarUsuariosError, (state,{ payload }) => ({
+        ...state,
+        loading: false,
+        loaded: false,
+        error: payload
+    })),
+
+);
+
+export function counterReducer(state, action) {
+    return _counterReducer(state, action);
+}
